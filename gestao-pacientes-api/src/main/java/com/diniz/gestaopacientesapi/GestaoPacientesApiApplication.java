5f222ec8646666de716c1e1b6d16ef54b2d4d479
Wilson Diniz Silva <wilson.diniz@outlook.com.br>
2023-09-15 15:12:00 -0300

    Arquivos iniciais API

---
+++ gestao-pacientes-api/src/main/java/com/diniz/gestaopacientesapi/GestaoPacientesApiApplication.java
@@ -0,0 +1,13 @@
+package com.diniz.gestaopacientesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPacientesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPacientesApiApplication.class, args);
+	}
+
+}
---
+++ gestao-pacientes-api/src/test/java/com/diniz/gestaopacientesapi/GestaoPacientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diniz.gestaopacientesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPacientesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
