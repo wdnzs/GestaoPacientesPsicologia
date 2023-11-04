33b7e4a404d0a1b87739213619faa81bee456048
Wilson Diniz Silva <wilson.diniz@outlook.com.br>
2023-11-04 11:15:02 -0300

    UP-30

--- gestao-pacientes-api/src/main/java/com/diniz/gestaopacientesapi/controller/PacienteController.java
+++ gestao-pacientes-api/src/main/java/com/diniz/gestaopacientesapi/controller/PacienteController.java
@@ -1,12 +1,14 @@
 package com.diniz.gestaopacientesapi.controller;
 
 import java.util.List;
+import java.util.function.Function;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,7 +23,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/pacientes")
 @AllArgsConstructor
-public class PacienteController {
+public class PacienteController<U> {
 
     private final PacienteRepository pacienteRepository;
 
@@ -42,5 +44,36 @@ public class PacienteController {
     public Paciente create(@RequestBody Paciente paciente) {
        return pacienteRepository.save(paciente);
      }
+    
+     /*
+      * Não ficou clara a exigência de inserir Function dentro do .map,
+      * incluindo o alerta no return.
+      */
+    @PutMapping("/{id}")
+    public U update(@PathVariable Long id, @RequestBody Paciente paciente){
+        return pacienteRepository.findById(id)
+            .map((Function<? super Paciente, ? extends U>) item -> {
+                item.setNome(paciente.getNome());
+                item.setDataNascimento(paciente.getDataNascimento());
+                item.setSexo(paciente.getSexo());
+                item.setEndereco(paciente.getEndereco());
+                item.setBairro(paciente.getBairro());
+                item.setCep(paciente.getCep());
+                item.setCidade(paciente.getCidade());
+                item.setEstado(paciente.getEstado());
+                item.setRg(paciente.getRg());
+                item.setCpf(paciente.getCpf());
+                item.setNomeMae(paciente.getNomeMae());
+                item.setNomePai(paciente.getNomePai());
+                item.setEstadoCivil(paciente.getEstadoCivil());
+                item.setProfissao(paciente.getProfissao());
+                item.setNacionalidade(paciente.getNacionalidade());
+
+                Paciente updated = pacienteRepository.save(item);
+                return (U) ResponseEntity.ok().body(updated);
+                
+            }).orElseThrow();
+
+    }
     
 }
